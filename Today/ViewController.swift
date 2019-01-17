75bd2824d235c0c72076f896fd6322be74f9dfcb
Saiyan <saiyan@LogsLocal.local>
2019-01-18 00:02:35 +0200

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Saiyan on 2019/01/18.
+//  Copyright © 2019 Rob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
